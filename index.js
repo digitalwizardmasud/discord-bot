585f5b0997c669f8ff70ab09345810f35615958c
masudpro121 <masud.pro.121@gmail.com>
2023-06-11 06:56:05 +0600

    embed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const axios = require("axios");
-const { Client, IntentsBitField, AttachmentBuilder } = require("discord.js");
+const {
+  Client,
+  IntentsBitField,
+  AttachmentBuilder,
+  Events,
+  EmbedBuilder,
+} = require("discord.js");
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -24,11 +30,33 @@ client.on("messageCreate", (msg) => {
   // msg.channel.send({ files: [attachment] });
 });
 
-client.on("interactionCreate",  interaction=>{
-  if(!interaction.isChatInputCommand()) return
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
   console.log(interaction.commandName);
-  console.log(interaction.options._hoistedOptions[0].value, 'prompt of command');
-  interaction.reply("Working for it")
-})
+  console.log(
+    interaction.options._hoistedOptions[0].value,
+    "prompt of command"
+  );
+  interaction.reply("Working for it");
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.author.bot) return;
+  const exampleEmbed = new EmbedBuilder()
+    .setTitle("Enjoy your Life")
+    .setURL("https://google.com")
+    .setColor('Random')
+    .setDescription(message.content)
+  const embed1 = new EmbedBuilder()
+  .setTitle("Embed1")
+    .setImage("https://i.ibb.co/zV0KSYq/free-photo-of-city-road-man-love.jpg")
+    .setURL("https://google.com")
+  const embed2 = new EmbedBuilder()
+    .setTitle("Embed2")
+    .setImage("https://i.ibb.co/8sjQqT0/8.png")
+    .setURL("https://google.com")
+
+  message.channel.send({embeds:[exampleEmbed, embed1, embed2]});
+});
 
 client.login(TOKEN);
